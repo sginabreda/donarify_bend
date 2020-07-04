f80055850d112d813c75f2705d26f66a7576022c
SANTIAGO GINABREDA <sginabreda@santandertecnologia.com.ar>
2020-07-04 18:39:46 -0300

    Initial commit

---
+++ src/main/kotlin/com/proyectofinal/donarify/DonarifyApplication.kt
@@ -0,0 +1,11 @@
+package com.proyectofinal.donarify
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DonarifyApplication
+
+fun main(args: Array<String>) {
+	runApplication<DonarifyApplication>(*args)
+}
