17d1096462e43709795cb6c5810590660d29d4a4
SANTIAGO GINABREDA <sginabreda@santandertecnologia.com.ar>
2020-07-11 19:27:59 -0300

    Adding exception internal handler to test Heroku deploy

--- src/main/kotlin/com/proyectofinal/donarify/exception/ExceptionHandler.kt
+++ src/main/kotlin/com/proyectofinal/donarify/exception/ExceptionHandler.kt
@@ -2,6 +2,7 @@ package com.proyectofinal.donarify.exception
 
 import com.proyectofinal.donarify.domain.ApiError
 import com.proyectofinal.donarify.logger
+import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
@@ -19,5 +20,10 @@ class ExceptionHandler: ResponseEntityExceptionHandler() {
         val errorDetails = ApiError(ex.message, ex.code)
         exLogger.error("Exception thrown - CODE: ${ex.code}  MESSAGE: \"${ex.message}\" STATUS: ${ex.status}")
         return ResponseEntity(errorDetails, HttpStatus.valueOf(ex.status))
+    }
+
+    override fun handleExceptionInternal(ex: Exception, body: Any?, headers: HttpHeaders, status: HttpStatus, request: WebRequest): ResponseEntity<Any> {
+        val errorDetails = ApiError("One or more parameters is either missing or incorrect", "bad.request")
+        return ResponseEntity(errorDetails, HttpStatus.BAD_REQUEST)
     }
 }
